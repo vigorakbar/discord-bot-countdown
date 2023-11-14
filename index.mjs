ce28967d7b38727d49e0fb84fd643ddfae9bfb63
Vigor Akbar <vigorakbar@gmail.com>
2023-11-14 22:47:00 +0800

    feat: send countdown message

--- index.mjs
+++ index.mjs
@@ -1,37 +1,62 @@
 import { Client, Events, GatewayIntentBits } from "discord.js";
 import cron from "cron";
 import dotenv from "dotenv";
+import moment from "moment-timezone";
 
 dotenv.config();
+
+const TIMEZONE = "Asia/Jakarta";
+
+moment.tz.setDefault(TIMEZONE);
+
+// sorted please :p
+const defaultMeetups = [
+  moment("2023-10-30"), // test before current time
+  moment("2023-11-17"),
+  moment("2023-12-30"),
+];
 
 const TOKEN = process.env.DISCORD_TOKEN;
 
 // const CLIENT_ID = "1173646712775127040";
 
-// Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-// When the client is ready, run this code (only once)
-// We use 'c' for the event parameter to keep it separate from the already defined 'client'
 client.once(Events.ClientReady, (c) => {
   console.log(`Ready! Logged in as ${c.user.tag}`);
 
   // TODO: adjust cron time
-  let scheduledMessage = new cron.CronJob("00 31 00 * * *", () => {
-    // This runs every day at 10:30:00, you can do anything you want
-    // Specifing your guild (server) and your channel
-    const guild = client.guilds.cache.get("1117145504925626390");
-    const channel = guild.channels.cache.get("1173660009016590437");
+  let scheduledMessage = new cron.CronJob(
+    "00 00 08 * * *",
+    () => {
+      const guild = client.guilds.cache.get("1117145504925626390");
+      const channel = guild.channels.cache.get("1173660009016590437");
 
-    // TODO: handle next dates and handle command to add dates
+      const upcomingMeetup = defaultMeetups.filter((meetup) =>
+        meetup.isAfter(moment().startOf("day"))
+      )[0];
+      if (!upcomingMeetup) return;
 
-    // TODO: send count down message
-    channel.send("Test message");
-  });
+      // TODO: handle next dates and handle command to add dates
+      const now = moment().startOf("day");
 
-  // When you want to start it, use:
+      const diffMonths = Math.abs(now.diff(upcomingMeetup, "months"));
+      const diffDays =
+        Math.abs(now.diff(upcomingMeetup, "days")) - diffMonths * 30;
+      const monthsMessage = diffMonths ? ` ${diffMonths} months and` : "";
+      let message = `Hello 😃
+It is${monthsMessage} ${diffDays} days left before the next date!
+Can't wait to see you 😊`;
+      if (diffMonths || diffDays) {
+        channel.send(message);
+      }
+    },
+    undefined,
+    undefined,
+    TIMEZONE
+  );
+
   scheduledMessage.start();
 });
 
-// Log in to Discord with your client's token
 client.login(TOKEN);
